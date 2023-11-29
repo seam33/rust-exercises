468108cfc98a925165fcf60372b1fc603bb583ce
seam33 <41659243+seam33@users.noreply.github.com>
2023-11-29 10:44:51 -0500

    :sparkles: Add variables exercise.

---
+++ exercise-1/src/main.rs
@@ -0,0 +1,18 @@
+/*
+    Description: This is the most simpliest program in rust. The are some examples
+    relatate to strings and numbers.
+
+    Note: By default all variables in rust are inmutable, if you want to change that behavior, 
+    it should use "mut".
+*/
+
+fn main() {
+    // By default all variable in rust are inmutable, if you want to change that behavior, it should use "mut"
+    let message = "Hello World";
+    let mut number = 25;
+
+    number = 26;
+
+    println!("This's my message -> '{}'", message);
+    println!("This's is a number -> '{}'", number);
+}
